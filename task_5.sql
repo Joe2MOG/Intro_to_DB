23c7d6f7b4ccb361e9764df42ca5fda9642b6fee
Joseph Albert <albertjoseph08066227697@gmail.com>
2024-10-19 17:58:07 +0100

    "Add task_5.sql for single data insertion in the customer table"

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,4 @@
--- Use the specified database
 USE alx_book_store;
 
--- Insert a single row into the Customer table
-INSERT INTO Customer (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
